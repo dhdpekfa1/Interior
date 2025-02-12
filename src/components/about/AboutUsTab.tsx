39754da9b966cc47558a5ac80ebb5aca1b01a354
닮 <dhdpekfa1@daum.net>
2025-02-13 01:33:11 +0900

    [feat] 회사 소개 레이아웃]

--- src/components/about/AboutUsTab.tsx
+++ src/components/about/AboutUsTab.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { SubTitle } from '@/components/common';
 
 export const AboutUsTab = () => {
-  return <div className='wrapper'>AboutUsTab</div>;
+  return (
+    <div className='wrapper'>
+      <SubTitle title='회사 소개' />
+      <div className='grid gap-4 w-full grid-cols-1 md:grid-cols-2'>
+        <div className='bg-cream h-[320px] md:h-[400px]'>사진</div>
+        <div className='bg-cream h-[320px] md:h-[400px]'>소개</div>
+      </div>
+    </div>
+  );
 };
