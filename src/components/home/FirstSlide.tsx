84ba8d02e1e16facdfcc2015bbc9e3bcce26a529
닮 <dhdpekfa1@daum.net>
2025-02-13 01:06:54 +0900

    [design] 홈 슬라이드 반응형

--- src/components/home/FirstSlide.tsx
+++ src/components/home/FirstSlide.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { menuData } from '@/assets/data';
+import { Button } from '@/components/ui';
 
 export const FirstSlide = () => {
   const data = menuData.find((data) => data.title === '제품소개');
@@ -9,25 +10,27 @@ export const FirstSlide = () => {
   }
 
   return (
-    <div className='w-full h-screen flex'>
+    <div className='w-full h-screen flex flex-col md:flex-row'>
       {data.subMenu.map((item) => (
         <div
           key={item.label}
-          className='group relative duration-500 ease-in-out w-full h-[20%] max-sm:hover:h-[40%] md:w-[20%] md:h-full md:hover:w-[40%] flex items-center justify-center'
+          className='group relative duration-500 ease-in-out w-full h-[20%] max-md:hover:h-[40%] md:w-[20%] md:h-full md:hover:w-[40%] flex items-center justify-center'
         >
           <div
             className='absolute inset-0 bg-cover bg-center transition-all duration-500'
             style={{ backgroundImage: `url(${item.img})` }}
           />
-          <div className='relative z-10 flex flex-col items-center justify-center text-white duration-300'>
-            <h2 className='text-2xl font-bold'>{item.label}</h2>
-            <Link
-              href={item.url}
-              className='mt-2 inline-block px-4 py-2 bg-second hover:bg-blue rounded-lg text-white text-sm'
-            >
+          <Link
+            href={item.url}
+            className='relative z-10 flex flex-col items-center justify-center text-white duration-300'
+          >
+            <h2 className='text-lg md:text-2xl font-bold group-hover:text-xl md:group-hover:text-3xl'>
+              {item.label}
+            </h2>
+            <Button className='mt-2 inline-block h-fit px-3 py-1 bg-second hover:bg-blue rounded-lg text-white text-xs md:text-sm'>
               제품 보기
-            </Link>
-          </div>
+            </Button>
+          </Link>
         </div>
       ))}
     </div>
