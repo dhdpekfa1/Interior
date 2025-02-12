070cfd0664fe21b2e259b6f3b4e2dee6f7c03f04
닮 <dhdpekfa1@daum.net>
2025-02-12 16:06:57 +0900

    [init] 초기 설정

---
+++ src/components/frame/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className='bottom-0 left-0 w-full bg-zinc-900 text-center py-4'>
+      Footer
+    </footer>
+  );
+};
---
+++ src/components/frame/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div className='fixed w-full h-14 bg-zinc-900 text-center py-4'>Header</div>
+  );
+};
