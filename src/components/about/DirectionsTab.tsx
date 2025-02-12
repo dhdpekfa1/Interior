8bb6627a1d2c4eea4aa4f687ef8dde0b8be7cf73
닮 <dhdpekfa1@daum.net>
2025-02-13 01:56:35 +0900

    [feat] 찾아오기 레이아웃

--- src/components/about/DirectionsTab.tsx
+++ src/components/about/DirectionsTab.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import { SubTitle } from '@/components/common';
 
 export const DirectionsTab = () => {
-  return <div className='wrapper'>DirectionsTab</div>;
+  // TODO: 클릭 시 바로가기 추가(주소 -> 길찾기, 전화번호 -> 전화)
+  const contact = [
+    { label: '주소', info: '인천광역시 남동구 3890' },
+    { label: '전화번호', info: '032-101-2020' },
+    { label: '팩스', info: '032-101-2020' },
+  ];
+  return (
+    <div className='wrapper'>
+      <SubTitle title='찾아오기' />
+      <div
+        className='bg-cream'
+        style={{
+          // 지도 크기
+          width: '100%',
+          height: 'calc(50vh)',
+        }}
+      >
+        지도
+      </div>
+
+      <div className='grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-4 md:mx-4 my-6'>
+        {contact.map((item) => (
+          <div
+            key={item.label}
+            className='flex flex-col items-center gap-3 bg-second/80 rounded-sm p-3 w-full'
+          >
+            <p className='text-base md:text-lg font-bold text-point'>
+              {item.label}
+            </p>
+            <div className='w-full h-0.5 bg-cream' />
+            <p className='text-base md:text-lg font-bold text-ef'>
+              {item.info}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
