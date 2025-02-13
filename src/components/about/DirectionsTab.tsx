d9f373a6e05b48de3b78b74d2d184dce329a1352
닮 <dhdpekfa1@daum.net>
2025-02-14 00:10:02 +0900

    [design] 빠른 문의 디자인

--- src/components/about/DirectionsTab.tsx
+++ src/components/about/DirectionsTab.tsx
@@ -3,7 +3,7 @@ import { SubTitle } from '@/components/common';
 export const DirectionsTab = () => {
   // TODO: 클릭 시 바로가기 추가(주소 -> 길찾기, 전화번호 -> 전화)
   const contact = [
-    { label: '주소', info: '서울특별시 서초구 쨔란 5번길 2 1014호0' },
+    { label: '주소', info: '인천시 서초구 5번길 114호' },
     { label: '전화번호', info: '032-101-2020' },
     { label: '팩스', info: '032-101-2020' },
   ];
@@ -21,13 +21,13 @@ export const DirectionsTab = () => {
         지도
       </div>
 
-      <div className='grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-4 md:mx-4 my-6'>
+      <div className='grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-4 md:mx-1 my-6'>
         {contact.map((item) => (
           <div
             key={item.label}
             className='flex flex-col items-center gap-3 bg-second/80 rounded-sm p-3 w-full'
           >
-            <p className='text-base md:text-lg font-bold text-point'>
+            <p className='text-base sm:text-sm md:text-lg font-bold text-point'>
               {item.label}
             </p>
             <div className='w-[85%] h-0.5 bg-cream' />
--- src/components/frame/QuickInquiry.tsx
+++ src/components/frame/QuickInquiry.tsx
@@ -12,19 +12,25 @@ export const QuickInquiry = () => {
       )}
     >
       {/* 회사 */}
-      <div className='flex gap-2 bg-blue rounded-t-md p-1 text-four'>
+      <a
+        href='tel:0321012020'
+        className='flex gap-2 bg-blue rounded-t-md p-1 text-four'
+      >
         <span className='flex gap-2 items-center'>
           <Contact className='h-auto w-3 sm:w-4' /> 빠른 견적 문의
         </span>
         <span>032-101-2020</span>
-      </div>
+      </a>
       {/* 핸드폰 */}
-      <div className='flex gap-2 bg-[#567C8D] rounded-b-md p-1 text-cream'>
+      <a
+        href='tel:01020203030'
+        className='flex gap-2 bg-[#567C8D] rounded-b-md p-1 text-cream'
+      >
         <span className='flex gap-2 items-center'>
           <Phone className='h-auto w-3 sm:w-4' /> 빠른 견적 문의
         </span>
         <span className=''>010-2020-3030</span>
-      </div>
+      </a>
     </div>
   );
 };
