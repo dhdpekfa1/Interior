8be6abe6ef3617844f7ea5d61b266899ea2fe39a
닮 <dhdpekfa1@daum.net>
2025-02-15 18:48:21 +0900

    [refactor] 데스크탑 해더 컴포넌트 분리

--- src/components/frame/Header.tsx
+++ src/components/frame/Header.tsx
@@ -1,13 +1,6 @@
 import Link from 'next/link';
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarTrigger,
-} from '@/components/ui';
 import { MobileHeader } from './MobileHeader';
-import { menuData } from '@/assets/navMenuData';
+import { DesktopMenubar } from './DesktopHeader';
 
 export const Header = () => {
   return (
@@ -17,27 +10,7 @@ export const Header = () => {
       </Link>
 
       {/* PC Header */}
-      <div className='hidden md:block'>
-        <Menubar className='border-none bg-point text-second'>
-          {menuData.map((menu) => (
-            <MenubarMenu key={menu.title}>
-              <MenubarTrigger>
-                <span className='font-bold'>{menu.title}</span>
-              </MenubarTrigger>
-              <MenubarContent className='bg-second/80 p-2'>
-                {menu.subMenu.map((subItem) => (
-                  <Link key={subItem.label} href={subItem.url || ''}>
-                    <MenubarItem className='text-white hover:bg-gray-100 transition'>
-                      {subItem.label}
-                    </MenubarItem>
-                  </Link>
-                ))}
-              </MenubarContent>
-            </MenubarMenu>
-          ))}
-        </Menubar>
-      </div>
-
+      <DesktopMenubar />
       {/* Mobile Header */}
       <MobileHeader />
     </div>
