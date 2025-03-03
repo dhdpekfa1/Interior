617a2b6c604be274e0dd3f0388e32249d38dec6e
닮 <dhdpekfa1@daum.net>
2025-03-03 18:03:13 +0900

    [design] 상품리스트 레이아웃 변경

--- src/components/common/SampleList.tsx
+++ src/components/common/SampleList.tsx
@@ -3,8 +3,7 @@
 import { useState } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import Image from 'next/image';
-import { SubTabs, SubTabsList, SubTabsTrigger } from '@/components/ui';
-import { SubTitle, SearchBar, Pagination } from '@/components/common';
+import { SubTitle, Pagination } from '@/components/common';
 
 interface Data {
   id: number;
@@ -15,6 +14,8 @@ interface Data {
 
 interface SampleListProps {
   title: string;
+  imgUrl: string;
+  content: string;
   categories: { label: string; value: string }[];
   dataList: Data[];
 }
@@ -23,77 +24,62 @@ const ITEMS_PER_PAGE = 20; // 페이지당 아이템 수
 
 export const SampleList = ({
   title,
-  categories,
+  // categories,
+  imgUrl,
   dataList,
+  content,
 }: SampleListProps) => {
-  const [selectedTab, setSelectedTab] = useState('전체');
   const [currentPage, setCurrentPage] = useState(1);
   const router = useRouter();
   const pathname = usePathname();
 
-  // 선택된 카테고리에 따라 데이터 필터링
-  const filteredProducts =
-    selectedTab === '전체'
-      ? dataList
-      : dataList.filter((data) => data.category === selectedTab);
-
   // 페이지네이션 적용: 현재 페이지의 데이터만 보여줌
-  const totalItems = filteredProducts.length;
+  const totalItems = dataList.length;
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const paginatedProducts = filteredProducts.slice(
+  const paginatedProducts = dataList.slice(
     startIndex,
     startIndex + ITEMS_PER_PAGE
   );
-
-  // 검색 핸들러
-  const handleSearch = (query: string) => {
-    console.log('검색어:', query);
-  };
 
   return (
     <div className='wrapper'>
       <SubTitle title={title} />
 
-      {/* 카테고리 탭 */}
-      <SubTabs
-        defaultValue='all'
-        onValueChange={(value) => {
-          const selectedLabel =
-            categories.find((c) => c.value === value)?.label || '전체';
-          setSelectedTab(selectedLabel);
-          setCurrentPage(1); // 카테고리 변경 시 1페이지로 리셋
-        }}
-      >
-        <SubTabsList>
-          {categories.map((category) => (
-            <SubTabsTrigger key={category.value} value={category.value}>
-              {category.label}
-            </SubTabsTrigger>
-          ))}
-        </SubTabsList>
-      </SubTabs>
-
-      {/* 검색창 */}
-      <div className='flex items-center justify-end mt-5 md:mt-7 mb-4'>
-        <SearchBar onSubmit={handleSearch} />
+      {/* 제품 설명 */}
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 items-center lg:items-end mb-10'>
+        <Image
+          src={imgUrl}
+          alt={`${title} 소개`}
+          width={200}
+          height={300}
+          className='w-full h-full'
+        />
+        <p className='text-xs sm:text-sm md:text-base lg:text-xl text-white break-keep lg:pb-8'>
+          {content}
+        </p>
       </div>
 
       {/* 제품 리스트 */}
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4'>
+      <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4'>
         {paginatedProducts.map((product) => (
           <div
             onClick={() => router.push(`${pathname}/${product.id}`)}
             key={product.id}
-            className='group transition-transform duration-300 hover:scale-105'
+            className='group transition-transform duration-300'
           >
-            <div className='border border-dd/50 p-1 text-center'>
+            <div>
               <div className='relative w-full aspect-square overflow-hidden'>
-                <Image src={product.img || ''} alt={product.name} fill />
+                <Image
+                  src={product.img || ''}
+                  alt={product.name}
+                  fill
+                  className='hover:scale-110 duration-300'
+                />
               </div>
+              <p className='mt-2 text-center text-dd text-xs sm:text-sm md:text-base'>
+                {product.name}
+              </p>
             </div>
-            <p className='mt-2 text-center text-dd text-sm sm:text-base font-bold'>
-              {product.name}
-            </p>
           </div>
         ))}
       </div>
--- src/components/product/DecoTab.tsx
+++ src/components/product/DecoTab.tsx
@@ -4,6 +4,7 @@ import ProductData from '@/assets/mock/mockProduct.json';
 import categories from '@/assets/categories.json';
 
 export const DecoTab = () => {
+  // TODO: 카테고리 사용 안할 경우 관련 코드 전부 제거
   // deco 탭 카테고리
   const decoCategories = categories.deco;
 
@@ -18,8 +19,12 @@ export const DecoTab = () => {
   return (
     <SampleList
       title='데코타일'
-      categories={decoCategories}
+      imgUrl='https://images.unsplash.com/photo-1529854140025-25995121f16f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29udGVudHxlbnwwfHwwfHx8MA%3D%3D'
+      content='폴리프로필렌은 분자구성이 안정적이며, 연소 및 분해시 유해가스의 발생이 없고, 강도 및 식품안정성이 우수하여 식품 포장재료로도 널리 사용 되는 소재 입니다.
+또한 PVC등 타 표면소재를 가공할 때 필요로 하는 가소제(Phthalate)를 전혀 사용하지 않아 환경호르몬과 새집 증후군의 원인물질인휘발성 유기화합물(VOCs)의 발생을 사전에 차단하고 있습니다.'
       dataList={decoProducts}
+      // 사용 안할 경우 prop도 제거
+      categories={decoCategories}
     />
   );
 };
