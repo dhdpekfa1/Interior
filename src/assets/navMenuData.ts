76c8976a53f77f1767d6447f38c3379900e63c17
닮 <dhdpekfa1@daum.net>
2025-03-03 16:13:36 +0900

    [feat] 홈 슬라이드 제거

--- src/assets/navMenuData.ts
+++ src/assets/navMenuData.ts
@@ -50,11 +50,11 @@ export const menuData: MenuItem[] = [
         url: '/product/deluxe',
         img: 'https://images.unsplash.com/photo-1580398562556-d33329a0f29b?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       },
-      {
-        label: '수평몰탈',
-        url: '/product/self',
-        img: 'https://images.unsplash.com/photo-1454694220579-9d6672b1ec2a?q=80&w=2185&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      },
+      // {
+      //   label: '수평몰탈',
+      //   url: '/product/self',
+      //   img: 'https://images.unsplash.com/photo-1454694220579-9d6672b1ec2a?q=80&w=2185&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      // },
     ],
     menuTitle: [
       '전체',
--- src/components/home/SecondSlide.tsx
+++ src/components/home/SecondSlide.tsx
@@ -1,5 +1,48 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { menuData } from '@/assets/navMenuData';
+import { Button } from '@/components/ui';
 
 export const SecondSlide = () => {
-  return <div>SecondSlide</div>;
+  const data = menuData.find((data) => data.title === '제품소개');
+
+  if (!data || !data.subMenu) {
+    return <div>데이터가 없습니다.</div>;
+  }
+
+  return (
+    <div className='w-full h-full grid grid-cols-1 md:grid-cols-5 gap-8 md:gap-4 px-4 py-10'>
+      {data.subMenu.map((item) => (
+        <Link
+          href={item.url}
+          key={item.label}
+          className='flex flex-col items-center gap-4 group'
+        >
+          {/* 이미지 */}
+          <div className='flex items-center justify-center w-full h-[180px] md:h-[250px] overflow-hidden rounded-md'>
+            <Image
+              src={item.img!}
+              alt={item.label}
+              width={300}
+              height={200}
+              className='w-[95%] sm:w-[90%] md:w-full h-full object-cover rounded-md transition-transform duration-300 ease-in-out group-hover:scale-105'
+            />
+          </div>
+
+          {/* 텍스트 + 버튼 */}
+          <div className='text-center'>
+            <h2 className='text-lg md:text-xl font-bold text-white'>
+              {item.label}
+            </h2>
+            <Button
+              className='mt-2 px-3 py-1 border bg-point/80 border-second/70 rounded-lg text-white text-xs md:text-sm 
+              transition-all duration-300 group-hover:bg-second/80'
+            >
+              제품 보기
+            </Button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
