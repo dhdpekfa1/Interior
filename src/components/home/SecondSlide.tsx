f60aeb242a2ff44399e656593f2fa6c133300b9c
닮 <dhdpekfa1@daum.net>
2025-03-03 17:24:28 +0900

    [design] 홈 SecondSlide gap 수정

--- src/components/home/SecondSlide.tsx
+++ src/components/home/SecondSlide.tsx
@@ -19,13 +19,13 @@ export const SecondSlide = () => {
           className='flex flex-col items-center gap-4 group'
         >
           {/* 이미지 */}
-          <div className='flex items-center justify-center w-full h-[180px] md:h-[250px] overflow-hidden rounded-md'>
+          <div className='flex items-center justify-center w-[85%] lg:w-full h-[180px] md:h-[380px] overflow-hidden rounded-md'>
             <Image
               src={item.img!}
               alt={item.label}
               width={300}
               height={200}
-              className='w-[95%] sm:w-[90%] md:w-full h-full object-cover rounded-md transition-transform duration-300 ease-in-out group-hover:scale-105'
+              className='w-full md:w-full h-full object-cover rounded-md transition-transform duration-300 ease-in-out group-hover:scale-110'
             />
           </div>
 
@@ -34,10 +34,7 @@ export const SecondSlide = () => {
             <h2 className='text-lg md:text-xl font-bold text-white'>
               {item.label}
             </h2>
-            <Button
-              className='mt-2 px-3 py-1 border bg-point/80 border-second/70 rounded-lg text-white text-xs md:text-sm 
-              transition-all duration-300 group-hover:bg-second/80'
-            >
+            <Button className='mt-2 px-3 py-1 border bg-point/80 border-second/70 rounded-lg text-white text-xs md:text-sm transition-all duration-300 group-hover:bg-second/80 mb-6'>
               제품 보기
             </Button>
           </div>
