301ae96d4ac45a0ed639b23b7effee89cb9ea647
닮 <dhdpekfa1@daum.net>
2025-03-05 00:14:14 +0900

    [design] 탭 스타일 변경

--- src/components/ui/tabs/tabs.tsx
+++ src/components/ui/tabs/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      'inline-flex h-8 sm:h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground',
+      'inline-flex h-8 sm:h-10 items-center justify-center rounded-none bg-ef p-1 text-muted-foreground',
       className
     )}
     {...props}
@@ -25,9 +25,9 @@ const TabsList = React.forwardRef<
         className={cn(
           'px-1 md:px-2 lg:px-5 -ml-2 py-4 sm:py-5 mr-1',
           'flex justify-center items-center w-full h-full px-1',
-          'rounded-tl-sm rounded-bl-sm',
+          'rounded-none',
           'font-extrabold text-sm md:text-lg lg:text-xl',
-          'bg-second text-point'
+          'bg-two text-white'
         )}
       >
         {title}
@@ -45,7 +45,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex items-center justify-center rounded whitespace-nowrap px-1 py-1 sm:px-2 sm:py-1.5 text-xs md:text-sm lg:text-base font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-second data-[state=active]:font-bold data-[state=active]:bg-point',
+      'inline-flex items-center justify-center whitespace-nowrap px-1 py-1 sm:px-2 sm:py-1.5 text-xs md:text-sm lg:text-base font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-two data-[state=active]:font-bold',
       className
     )}
     {...props}
