0e7ba546af40588feea72c2e7a46adeb72149eee
닮 <dhdpekfa1@daum.net>
2025-03-05 16:08:01 +0900

    [design] error, not-found 페이지 색상 변경

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -16,18 +16,18 @@ const ErrorPage = ({ error, reset }: { error: Error; reset: () => void }) => {
     <div className='container flex flex-col items-center justify-center h-screen text-center'>
       {/* ERROR 텍스트 */}
       <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold tracking-widest flex items-center gap-2'>
-        <span className='flex items-center text-second'>
+        <span className='flex items-center text-point'>
           ERR
           <AlertTriangle />R
         </span>
-        <span className='text-ef flex items-center'>PAGE</span>
+        <span className='text-point/90 flex items-center'>PAGE</span>
       </h1>
 
       {/* 안내 문구 */}
-      <p className='mt-4 text-ef/60 text-base md:text-lg lg:text-xl'>
+      <p className='mt-4 text-two/70 text-base md:text-lg lg:text-xl'>
         죄송합니다. 페이지를 불러오는 중 오류가 발생했습니다.
       </p>
-      <p className='text-ef/40 mt-2 text-xs md:text-base lg:text-lg break-keep'>
+      <p className='text-two/70 mt-2 text-xs md:text-base lg:text-lg break-keep'>
         <span className='hidden sm:inline'>일시적인 문제가 발생했거나,</span>{' '}
         네트워크 연결이 원활하지 않을 수 있습니다.
       </p>
@@ -36,13 +36,13 @@ const ErrorPage = ({ error, reset }: { error: Error; reset: () => void }) => {
       <div className='mt-6 flex gap-4'>
         <Button
           onClick={() => reset()}
-          className='border border-ef text-ef h-fit px-3 y-1 md:px-5 md:py-2 rounded-md hover:bg-ef/20 hover:text-white transition bg-point text-xs md:text-base'
+          className='border border-ef text-ef h-fit px-3 y-1 md:px-5 md:py-2 rounded-md hover:bg-point hover:text-white transition bg-point/90 text-xs md:text-base'
         >
           다시 시도
         </Button>
         <Button
           onClick={() => router.replace('/')}
-          className='border bg-ef/90 border-point text-point h-fit px-4 y-1 md:px-6 md:py-2 rounded-md hover:bg-ef hover:text-point transition text-xs md:text-base lg:text-lg'
+          className='border bg-white border-point text-point h-fit  px-4 y-1 md:px-6 md:py-2 rounded-md hover:bg-ef hover:text-point transition text-xs md:text-base lg:text-lg'
         >
           홈
         </Button>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,20 +11,20 @@ const NotFoundPage = () => {
     <div className='container flex flex-col items-center justify-center h-screen text-center'>
       {/* 404 ERROR 텍스트 */}
       <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold tracking-widest flex items-center gap-2'>
-        <span className='flex items-center text-second'>
+        <span className='flex items-center text-point'>
           4<Ban />4
         </span>
-        <span className='text-ef flex items-center'>
+        <span className='text-point/90 flex items-center'>
           ERR
           <Ban />R
         </span>
       </h1>
 
       {/* 안내 문구 */}
-      <p className='mt-4 text-ef/60 text-base md:text-lg lg:text-xl'>
+      <p className='mt-4 text-two/70 text-base md:text-lg lg:text-xl'>
         죄송합니다. 현재 요청하신 페이지를 찾을 수 없습니다.
       </p>
-      <p className='text-ef/40 mt-2 text-xs md:text-base lg:text-lg break-keep'>
+      <p className='text-two/70 mt-2 text-xs md:text-base lg:text-lg break-keep'>
         <span className='hidden sm:inline'>
           존재하지 않는 주소를 입력했거나,
         </span>
@@ -35,13 +35,13 @@ const NotFoundPage = () => {
       <div className='mt-6 flex gap-4'>
         <Button
           onClick={() => router.back()}
-          className='border border-ef text-ef h-fit px-3 y-1 md:px-5 md:py-2 rounded-md hover:bg-ef/20 hover:text-white transition bg-point text-xs md:text-base'
+          className='border border-ef text-ef h-fit px-3 y-1 md:px-5 md:py-2 rounded-md hover:bg-point hover:text-white transition bg-point/90 text-xs md:text-base'
         >
           이전
         </Button>
         <Button
           onClick={() => router.replace('/')}
-          className='border bg-ef/90 border-point text-point h-fit  px-4 y-1 md:px-6 md:py-2 rounded-md hover:bg-ef hover:text-point transition text-xs md:text-base lg:text-lg'
+          className='border bg-white border-point text-point h-fit  px-4 y-1 md:px-6 md:py-2 rounded-md hover:bg-ef hover:text-point transition text-xs md:text-base lg:text-lg'
         >
           홈
         </Button>
