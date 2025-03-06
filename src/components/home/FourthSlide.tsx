b5ac42885f2cda08920299c6ee955b5be20fc061
닮 <dhdpekfa1@daum.net>
2025-03-06 11:17:29 +0900

    [design] FourthSlide 레이아웃 수정

--- src/components/home/FourthSlide.tsx
+++ src/components/home/FourthSlide.tsx
@@ -4,19 +4,18 @@ import { KakaoMap } from '../common';
 export const FourthSlide = () => {
   return (
     <div className='m-8'>
-      <div className='flex flex-col sm:flex-row gap-2'>
+      <div className='flex flex-col sm:flex-row gap-6 sm:gap-2'>
         <KakaoMap />
-
+        {/* TODO: lg: 교통편 추가? */}
         <div className='flex items-center justify-center w-full'>
           <p className='text-two text-base md:text-lg font-semibold text-center leading-relaxed'>
             <span className='text-point font-bold'>신뢰</span>와{' '}
-            <span className='text-point font-bold'>진심</span>을 바탕으로
-            <br />
-            언제나 최선을 다하는
+            <span className='text-point font-bold'>진심</span>을 바탕으로 언제나
+            최선을 다하는
             <br />
             <span className='text-point font-bold'>YD-Industry</span>입니다.
             <br />
-            <br />
+            <br className='hidden sm:block' />
             함께 성장하며,{' '}
             <span className='text-point font-bold'>
               믿고 맡길 수 있는 파트너
