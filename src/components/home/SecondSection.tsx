0c0bc150add5832045c1afd5a7e72a3e82f81eea
닮 <dhdpekfa1@daum.net>
2025-03-06 12:44:08 +0900

    [feat] 홈 애니메이션 변경

--- src/components/home/SecondSection.tsx
+++ src/components/home/SecondSection.tsx
@@ -38,52 +38,62 @@ export const SecondSection = () => {
       <div className='w-full flex flex-wrap justify-center gap-8 px-4'>
         {data.subMenu.map((item, index) => (
           <InView key={item.label} threshold={0.2}>
-            {({ ref, inView }) => (
-              <Link
-                href={item.url}
-                ref={ref}
-                className={cn(
-                  'flex flex-col items-center gap-4 group transition-all duration-700 ease-out',
-                  index < 3 ? 'w-[90%] md:w-[30%]' : 'w-[90%] md:w-[30%]',
-                  inView
-                    ? 'opacity-100 translate-y-0 md:translate-y-0 md:translate-x-0'
-                    : 'opacity-0 translate-y-10 md:translate-y-0 md:translate-x-10'
-                )}
-              >
-                {/* 이미지 */}
-                <div className='flex items-center justify-center w-full h-[30rem] md:h-[380px] overflow-hidden'>
-                  <Image
-                    src={item.img!}
-                    alt={item.label}
-                    width={300}
-                    height={200}
-                    priority
-                    className='w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110'
-                  />
-                </div>
+            {({ ref, inView }) => {
+              const delay =
+                typeof window !== 'undefined' && window.innerWidth >= 768
+                  ? `${index * 200}ms`
+                  : '0ms';
 
-                {/* 텍스트 + 버튼 */}
-                <div className='flex flex-col items-center gap-2'>
-                  <h2 className='text-lg md:text-xl font-bold text-two'>
-                    {item.label}
-                  </h2>
-                  <span className='text-sm md:text-base w-[65%] break-keep text-center text-three'>
-                    {item.description}
-                  </span>
-                  <Button
-                    variant='outline'
-                    className={cn(
-                      'group flex items-center justify-center',
-                      'mt-2 px-3 py-1 border bg-white/80 border-two/50 text-two/50 text-xs md:text-sm transition-all duration-300 mb-6',
-                      'group-hover:border-point group-hover:text-point hover:bg-white/80'
-                    )}
-                  >
-                    제품 보기
-                    <MoveRight className='ml-1 text-base text-two/50 transition-transform duration-300 group-hover:translate-x-1 group-hover:text-point' />
-                  </Button>
-                </div>
-              </Link>
-            )}
+              return (
+                <Link
+                  href={item.url}
+                  ref={ref}
+                  className={cn(
+                    'flex flex-col items-center gap-4 group transition-all duration-700 ease-out',
+                    index < 3 ? 'w-[90%] md:w-[30%]' : 'w-[90%] md:w-[30%]',
+                    inView
+                      ? 'opacity-100 translate-y-0 md:translate-y-0 md:translate-x-0'
+                      : 'opacity-0 translate-y-10 md:translate-y-0 md:translate-x-10'
+                  )}
+                  style={{
+                    transitionDelay: delay,
+                  }}
+                >
+                  {/* 이미지 */}
+                  <div className='flex items-center justify-center w-full h-[30rem] md:h-[380px] overflow-hidden'>
+                    <Image
+                      src={item.img!}
+                      alt={item.label}
+                      width={300}
+                      height={200}
+                      priority
+                      className='w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110'
+                    />
+                  </div>
+
+                  {/* 텍스트 + 버튼 */}
+                  <div className='flex flex-col items-center gap-2'>
+                    <h2 className='text-lg md:text-xl font-bold text-two'>
+                      {item.label}
+                    </h2>
+                    <span className='text-sm md:text-base w-[65%] break-keep text-center text-three'>
+                      {item.description}
+                    </span>
+                    <Button
+                      variant='outline'
+                      className={cn(
+                        'group flex items-center justify-center',
+                        'mt-2 px-3 py-1 border bg-white/80 border-two/50 text-two/50 text-xs md:text-sm transition-all duration-300 mb-6',
+                        'group-hover:border-point group-hover:text-point hover:bg-white/80'
+                      )}
+                    >
+                      제품 보기
+                      <MoveRight className='ml-1 text-base text-two/50 transition-transform duration-300 group-hover:translate-x-1 group-hover:text-point' />
+                    </Button>
+                  </div>
+                </Link>
+              );
+            }}
           </InView>
         ))}
       </div>
