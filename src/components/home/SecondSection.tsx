db194fa5e32ddc241e49f19b63f62202a6fb2f49
닮 <dhdpekfa1@daum.net>
2025-04-04 18:37:36 +0900

    [refacotr] server 컴포넌트 내부 client 로직 컴포넌트로 분리

--- src/components/home/SecondSection.tsx
+++ src/components/home/SecondSection.tsx
@@ -1,20 +1,10 @@
-'use client';
+import { SecondSectionClient, SectionHeader } from '@/components/home';
+import { getSubMenuData } from '@/app/api/navigationMenu';
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { cn } from '@/lib/utils';
-import { InView } from 'react-intersection-observer';
-import { MoveRight } from 'lucide-react';
-import { Button } from '@/components/ui';
-import { SectionHeader } from '@/components/home';
-import { useProductMenuFetcher } from '@/api/useProductMenuFetcher';
+export const SecondSection = async () => {
+  const subMenuData = await getSubMenuData();
 
-export const SecondSection = () => {
-  const { subMenuData, loading } = useProductMenuFetcher();
-
-  console.log(subMenuData);
-
-  if (!subMenuData || loading) {
+  if (!subMenuData) {
     return (
       <div className='w-full flex flex-wrap justify-center gap-8 px-4 py-10'>
         {[...Array(5)].map((_, index) => (
@@ -38,58 +28,7 @@ export const SecondSection = () => {
         description='최고의 품질과 기술력으로 완성된 제품을 소개합니다.'
       />
       <div className='w-full flex flex-wrap justify-center gap-4 md:gap-8 px-4'>
-        {subMenuData.map((item, index) => (
-          <InView key={item.label} threshold={0.2}>
-            {({ ref, inView }) => {
-              return (
-                <Link
-                  href={item.url}
-                  ref={ref}
-                  className={cn(
-                    'flex flex-col items-center gap-4 group transition-all duration-700 ease-out',
-                    index < 3 ? 'w-[90%] md:w-[30%]' : 'w-[90%] md:w-[30%]',
-                    inView
-                      ? 'opacity-100 translate-y-0 md:translate-y-0 md:translate-x-0'
-                      : 'opacity-0 translate-y-10 md:translate-y-0 md:translate-x-10'
-                  )}
-                >
-                  {/* 이미지 */}
-                  <div className='flex items-center justify-center w-full h-[20rem] md:h-[380px] lg:h-[480px] overflow-hidden'>
-                    <Image
-                      src={item.img!}
-                      alt={item.label}
-                      width={300}
-                      height={200}
-                      priority
-                      className='w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110'
-                    />
-                  </div>
-
-                  {/* 텍스트 + 버튼 */}
-                  <div className='flex flex-col items-center gap-2'>
-                    <h2 className='text-lg md:text-xl font-bold text-two'>
-                      {item.label}
-                    </h2>
-                    <span className='text-sm md:text-base w-[65%] break-keep text-center text-three'>
-                      {item.description}
-                    </span>
-                    <Button
-                      variant='outline'
-                      className={cn(
-                        'group flex items-center justify-center',
-                        'mt-2 px-3 py-1 border bg-white/80 border-two/50 text-two/50 text-xs md:text-sm transition-all duration-300 mb-6',
-                        'group-hover:border-point group-hover:text-point hover:bg-white/80'
-                      )}
-                    >
-                      제품 보기
-                      <MoveRight className='ml-1 text-base text-two/50 transition-transform duration-300 group-hover:translate-x-1 group-hover:text-point' />
-                    </Button>
-                  </div>
-                </Link>
-              );
-            }}
-          </InView>
-        ))}
+        <SecondSectionClient subMenuData={subMenuData} />
       </div>
     </>
   );
