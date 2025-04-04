167efb1b607a5c6da899477043488cf91c11239c
닮 <dhdpekfa1@daum.net>
2025-04-04 11:13:12 +0900

    [feat] footer props 변경

--- src/components/frame/Footer.tsx
+++ src/components/frame/Footer.tsx
@@ -1,13 +1,6 @@
-'use client';
-
-import React from 'react';
 import { cn } from '@/lib/utils';
-import { CompanyInfo, CompanyInfoType } from '@/assets/companyInfo';
 
-const leftFooterData: CompanyInfoType[] = CompanyInfo.slice(0, 4);
-const rightFooterData: CompanyInfoType[] = CompanyInfo.slice(4, 8);
-
-interface InfoType {
+interface CompanyInfoType {
   id: string;
   label: string;
   content: string;
@@ -15,8 +8,13 @@ interface InfoType {
   direction: 'left' | 'right';
 }
 
-export const Footer = ({ companyInfo }: { companyInfo: InfoType[] }) => {
-  console.log(companyInfo);
+export const Footer = ({ companyInfo }: { companyInfo: CompanyInfoType[] }) => {
+  const leftFooterData = companyInfo.filter(
+    (info) => info.direction === 'left'
+  );
+  const rightFooterData = companyInfo.filter(
+    (info) => info.direction === 'right'
+  );
 
   return (
     <footer
@@ -27,7 +25,7 @@ export const Footer = ({ companyInfo }: { companyInfo: InfoType[] }) => {
     >
       <div className='flex flex-col gap-2'>
         {leftFooterData.map((item) => (
-          <div key={item.label} className='flex gap-2 text-sm md:text-base'>
+          <div key={item.label} className='flex gap-2'>
             <span className='text-white/70 w-20'>{item.label}</span>
             {item.label === '주소' ? (
               <a
@@ -49,12 +47,12 @@ export const Footer = ({ companyInfo }: { companyInfo: InfoType[] }) => {
         ))}
       </div>
 
-      <div className='flex flex-col gap-2 mt-6 md:mt-0'>
+      <div className='flex flex-col gap-2'>
         {rightFooterData.map((item) => (
-          <div key={item.label} className='flex gap-2 text-sm md:text-base'>
+          <div key={item.label} className='flex gap-2'>
             <span className='text-white/70 w-24'>{item.label}</span>
             {item.label === 'TEL' ? (
-              <a href='tel:0313346771' className='font-bold'>
+              <a href='tel:0324723661' className='font-bold'>
                 {item.content}
               </a>
             ) : (
