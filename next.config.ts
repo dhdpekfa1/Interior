abe131fde82d865e59e605c192bb230d13f2bc19
닮 <dhdpekfa1@daum.net>
2025-05-06 20:33:43 +0900

    style: 홈 메인 베너 이미지 변경

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'images.unsplash.com',
-      // },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'plus.unsplash.com',
-      // },
       {
         protocol: 'http',
         hostname: '**',
